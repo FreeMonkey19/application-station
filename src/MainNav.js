83bf0087cce14b1c1e3bbf17f417bd90f194106f
freeMonkey19 <freeMonkey19@gmail.com>
2020-07-11 21:01:26 -0700

    create mainNav component with css and configure eslint

---
+++ src/MainNav.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./MainNav.css";
+import { NavLink } from "react-router-dom";
+
+export class MainNav extends Component {
+  render() {
+    return (
+      <nav className="main-nav" title="main-nav-bar">
+        <span className="nav-link-container" title="main-nav-link">
+          <NavLink className="nav-link" to="/" exact>
+            Home
+          </NavLink>
+        </span>
+        <span className="nav-link-container" title="main-nav-link">
+          <NavLink className="nav-link" to="/sign_in">
+            Sign In
+          </NavLink>
+        </span>
+
+        <span className="nav-link-container" title="main-nav-link">
+          <NavLink className="nav-link" to="/dashboard">
+            Dashboard
+          </NavLink>
+        </span>
+        <span className="nav-link-container" title="main-nav-link">
+          <NavLink className="nav-link" to="/search_form">
+            Job Search Form
+          </NavLink>
+        </span>
+        <span className="nav-link-container" title="main-nav-link">
+          <NavLink className="nav-link" to="/search_results">
+            Search Results
+          </NavLink>
+        </span>
+        <span className="nav-link-container" title="main-nav-link">
+          <NavLink className="nav-link" to="/users">
+            User List
+          </NavLink>
+        </span>
+      </nav>
+    
+    );
+  }
+}
+
+export default MainNav;
